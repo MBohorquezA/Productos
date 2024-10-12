84fd49f3eed268184e053f76b26a5911f6e4811e
Miguel Bohorquez <you@example.com>
2024-10-12 13:09:52 -0500

    rm Project api

--- Productos.Api/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Productos.Api.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
